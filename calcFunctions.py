3fa1b8195333814fc264f81b487dc11e75aa29f3
Myeonghwan Kim <myeonghwan2@kookmin.ac.kr>
2018-10-30 10:48:05 +0900

    del

--- calcFunctions.py
+++ calcFunctions.py
@@ -1,6 +1,4 @@
 from math import factorial as fact
-roman = {1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100:'C', 90:'XC',
-                  50:'L', 40:'XL',10:'X', 9:'IX', 5:'V', 4:'IV', 1:'I'}
 romans = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'),
           (50, 'L'), (40, 'XL'),(10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
 numBreak = [x[0] for x in romans]
