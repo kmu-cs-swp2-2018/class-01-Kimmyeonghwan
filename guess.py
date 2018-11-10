90fe8b8e7bd336b36fd371499b279f13806c5497
Myeonghwan Kim <myeonghwan2@kookmin.ac.kr>
2018-11-10 18:26:07 +0900

    guess 파일이 저장이 안됨 + 다 날라가서 다시 작성 (멘탈 붕괴)
    testGuess를 guess에 맞게 수정. (list 사용했으니 ListEqual 사용)

--- guess.py
+++ guess.py
@@ -2,35 +2,40 @@ class Guess:
 
     def __init__(self, word):
         self.numTries = 0
-        self.guessedChars = []
         self.secretWord = word
         self.life = 0
         # 사용자 편의를 위해 - 를 _ 로 변경
         self.currentStatus = "_"*len(self.secretWord)
+        # 집합 vs 리스트, 리스트에 append 해주는 방식을 이용함. 코드 간결화
+        self.guessedChars = []
 
-
-    def display(self):
-        print("Current :", self.currentStatus)
-        print("Tries : ", self.numTries)
-        print("Guessd Chars : ", self.guessedChars)
 
 
     def guess(self, character):
-        # 단어가 추측했던 단어(guessedChars)에 없다면 추가
-        if character not in self.guessedChars:
-            self.guessedChars.append(character)
+        self.guessedChars.append(character)
+        self.numTries = len(self.guessedChars)
 
-        # 단어가 정답의 단어와 다르면 목숨 -1
+        # 단어가 정답의 단어와 다르면 False 리턴
         if character not in self.secretWord:
-            self.life += 1
+            return False
 
         # 단어가 정답의 단어와 같으면
         else:
             for i in range(len(self.secretWord)):
                 if self.secretWord[i] == character:
                     self.currentStatus = self.currentStatus[:i] + character + self.currentStatus[i+1:]
+            return True
 
-            # 단어를 다 맞추면 True 리턴
-            if self.secretWord == self.currentStatus:
-                return True
-        self.numTries = len(self.guessedChars)
+    # 단어를 다 맞추면 True 리턴
+    def finished(self):
+        if self.secretWord == self.currentStatus:
+            return True
+        else:
+            return False
+
+    def displayCurrent(self):
+        return self.currentStatus
+
+
+    def displayGuessed(self):
+        return sorted(self.guessedChars)
--- testGuess.py
+++ testGuess.py
@@ -18,11 +18,22 @@ class TestGuess(unittest.TestCase):
         self.assertEqual(self.g1.displayCurrent(), '___a__t')
         self.g1.guess('u')
         self.assertEqual(self.g1.displayCurrent(), '___au_t')
+        # 한글 입력 방지 확인
+        self.g1.guess('ㅁ')
+        self.assertEqual(self.g1.displayCurrent(), '___au_t')
 
     def testDisplayGuessed(self):
 
         self.g1.guess('a')
-        self.assertIn('a', self.g1.displayGuessed())
+        self.assertListEqual(['a'], self.g1.displayGuessed())
+        self.g1.guess('t')
+        self.assertListEqual(['a', 't'], self.g1.displayGuessed())
+        self.g1.guess('u')
+        self.assertListEqual(['a', 't', 'u'], self.g1.displayGuessed())
+        # sorted 정상 작동 확인
+        self.g1.guess('d')
+        self.assertListEqual(['a', 'd', 't', 'u'], self.g1.displayGuessed())
+
 
 
 if __name__ == '__main__':
